51a7d5f97e8140383e2ca344dc09362926aba550
omar86556 <omarfarouq@outlook.fr>
2024-07-11 13:37:31 +0100

    update

--- warmup01.js
+++ warmup01.js
@@ -3,16 +3,16 @@
 
 // function declaration
 function MyPartnerName(firstname, lastname) {
-	return (firstname+" "+lastname )
+	return (firstname + " " + lastname)
 }
 
 // 2
-let MultipleOf13orNot = function(n) {
-	if(n % 13 === true){
-		return(n ," is a multiple of thirteen")
+let MultipleOf13orNot = function (n) {
+	if (n % 13 === 0) {
+		return (n + " is a multiple of thirteen")
 	}
-	else{
-		return(n , " isn't a multiple of thirteen")
+	else {
+		return (n + " isn't a multiple of thirteen")
 	}
 }
 
@@ -22,24 +22,20 @@ let MultipleOf13orNot = function(n) {
 
 // 3-create a function that takes a age as a parametre and return that age in seconds.*//
 
-const  AgeInSeconds = (age) =>  {
-	let days = age / 365
-	let hours = days / 24
-	let minuts = hours / 60
-	return (minuts / 60 ," is your age in seconds")
+const AgeInSeconds = (age) => {
+	let days = age * 365
+	let hours = days * 24
+	let minuts = hours * 60
+	return (minuts / 60 + " is your age in seconds")
 }
 //  * 4 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
 //  */
-const sameLength=(string1, string2)=> {
-	if (string1 === string2) {
-		return true
-	}
-	else {
-		return false
-	}
+const sameLength = (string1, string2) => {
+	if (string1.length === string2.length) { return true }
+	else { return false }
 }
 /**
- *? 5 - Write a function called (reverseStr) that takes a string as an input and returns the reverse of each letter
+*? 5 - Write a function called (reverseStr) that takes a string as an input and returns the reverse of each letter
  *? followed by a number starting from zero(solve it using while loop).
  *? example:    reverseStr('hello'); "o0l1l2e3h4"
  *
@@ -47,10 +43,11 @@ const sameLength=(string1, string2)=> {
  * @return {string}
  */
 
-let reverseStr = function (str){
-	let string =  ""
+
+let reverseStr = function (str) {
+	let string = ""
 	let i = 0
-	while (i <= str.length){
+	while (i <= str.length) {
 		string.concat()
 	}
 }
@@ -58,45 +55,46 @@ let reverseStr = function (str){
 // * 6 Write a function called laugh() that takes one parameter,
 // * num that represents the number of "ha"s to return.
 // * laugh(6); => "hahahahahaha"
- *
- * @function laugh
- * @param {number} n
- * @return {string}
- */
-const laugh= function (num){
-	let laugh = ""
+*
+* @function laugh
+* @param {number} n
+* @return {string}
+*/
+const laugh = function (num) {
+	let laugh2 = ""
 	let i = 0
 	while (i <= num) {
-		laugh.concat("ha")
+		laugh2 += "ha"
 		i++
 	}
+	return (laugh2)
 }
 
 
 /**
- * 7- write a function to Reverse an array without use array.reverse() method:
- * ex: reverseArray([1,2,3]) ==> [3,2,1]
- * @function reverseArray
- * @param {array} array
- * @return {Array}
- */
+* 7- write a function to Reverse an array without use array.reverse() method:
+* ex: reverseArray([1,2,3]) ==> [3,2,1]
+* @function reverseArray
+* @param {array} array
+* @return {Array}
+*/
 
-let reverseArray = (array ) => {}
+let reverseArray = (array) => { }
 
 
 /**
- * 8-Given an array of integers and an integer n.
- * find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
- * If n is not in the given array, return an empty array [].
- * Assume that n and all values in the given array will always be integers.
- * Example:
- * findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
- *
- * @param {Array} array
- * @param {number} n
- * @return {Array}
- */
-let findAll = (arr,n) => {}
+* 8-Given an array of integers and an integer n.
+* find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+* If n is not in the given array, return an empty array [].
+* Assume that n and all values in the given array will always be integers.
+* Example:
+* findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+*
+* @param {Array} array
+* @param {number} n
+* @return {Array}
+*/
+let findAll = (arr, n) => { }
 
 
 // 9. write a function that takes a string as an argument
@@ -106,6 +104,5 @@ let findAll = (arr,n) => {}
 //
 // Note : you can`t use any built-in functions .
 
-let reverseWord=(string) => {
-
+let reverseWord = (string) => {
 }
